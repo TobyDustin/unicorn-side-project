a7146a734c4828b166952bec61eb1e5fc1bfd816
Toby Dustin <zz>
2021-02-22 16:50:44 +0000

    feat: persistant data added

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-let tag = "";
-let ticket = "";
-let message = "";
+let tag = localStorage.getItem("tag") ?? "feat";
+let ticket = localStorage.getItem("ticket") ?? "gtech-00000";
+let message = localStorage.getItem("message") ?? "";
 
 let tagElement = null;
 let ticketElement = null;
@@ -11,13 +11,24 @@ function load() {
   ticketElement = document.getElementById("ticket");
   messageElement = document.getElementById("message");
 
+  fillForm();
   displayOutput();
+}
+function fillForm() {
+  tagElement.value = tag;
+  ticketElement.value = ticket;
+  messageElement.value = message;
 }
 
 function updateForm() {
   tag = tagElement.value;
   ticket = ticketElement.value.toLowerCase();
   message = messageElement.value;
+
+  localStorage.setItem("tag", tag);
+  localStorage.setItem("ticket", ticket);
+  localStorage.setItem("message", message);
+
   displayOutput();
 }
 
