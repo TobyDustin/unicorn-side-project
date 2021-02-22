f5b4952115427db1f07996a31ecc33e41ed1844e
Toby Dustin <zz>
2021-02-22 16:34:45 +0000

    feat: get all elements from dom

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-function init() {}
+let tag = "";
+let ticket = "";
+let message = "";
+
+let tagElement = null;
+let ticketElement = null;
+let messageElement = null;
+
+function load() {
+  tagElement = document.getElementById("tag");
+  ticketElement = document.getElementById("ticket");
+  messageElement = document.getElementById("message");
+}
